3f7b6e92f4ca6b2ad499edb86c724d2af298e78c
aquamanx98 <146301642+aquamanx98@users.noreply.github.com>
2024-10-02 21:31:26 +0000

    adicionando pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Oi meu amor, amo você!<br></br>
+      Espero que tenha dormido bem.<br></br>I just wanna be yours
+    </h1>
+  );
+}
+
+export default Home;
